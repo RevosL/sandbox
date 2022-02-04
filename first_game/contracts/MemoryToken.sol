9b5789ed0cdc9d6ddf5437599f901358b2f100cc
Bruno <planet.gamesextreme@gmail.com>
2022-02-04 13:38:17 -0300

    tokenMTT

--- first_game/contracts/MemoryToken.sol
+++ first_game/contracts/MemoryToken.sol
@@ -3,14 +3,18 @@ SPDX-License-Identifier: MIT
 */
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MemoryToken is ERC721URIStorage {
 
-    using Counter for Counter.Counter;
+    using Counters for Counters.Counter;
 
-    Counters.counter private _tokenIdCounter;
+    Counters.Counter private _tokenIdCounter;
 
+    constructor() ERC721("Memory Token", "MTT") {}
 
+    function mint(address wallet, string memory _tokenURI) public returns(uint256) {
+
+    }
 }
