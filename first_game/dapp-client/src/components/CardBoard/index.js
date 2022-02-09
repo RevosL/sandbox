e16dd27e3a2b662979a09f5484a7967fa7681c74
Bruno <planet.gamesextreme@gmail.com>
2022-02-09 01:39:38 -0300

    CardBoard

--- first_game/dapp-client/src/components/CardBoard/index.js
+++ first_game/dapp-client/src/components/CardBoard/index.js
@@ -2,21 +2,41 @@ const CardBoard = (props) => {
 
     const { cardData } = props
 
+    const { cardData, cardsWon, cardsChosenId, cardsChosen } = props.memory
+
+    const chooseImage = (cardId) => {
+        const cardIDstr = cardId.toString()
+
+        if(cardsWon.includes(cardIdStr)) {
+            return window.location.origin + '/images/white.png'
+        }
+        else if(cardsChosenId.includes(cardIdStr)) {
+            return cardData[cardIdStr].img
+        } else {
+            return window.location.origin + '/images/blank.png'
+        }
+    }
+
     return (
         cardData.map((card, key) => {
-            <img
-            key={key}
-            src={chooseImage(key)}
-            data-id={key}
-            onClick={(event) => {
-                let cardId = event.target.getAttribute('data-id')
+            return (
+                <img
+                key={key}
+                src={chooseImage(key)}
+                data-id={key}
+                onClick={(event) => {
+                    let cardId = event.target.getAttribute('data-id')
 
-                if(!checkAlreadyWonCard(cardId)) {
-                    handleFlipCard(cardId)
-                }
-            }}
-            />
+                    if(!checkAlreadyWonCard(cardId)) {
+                        handleFlipCard(cardId)
+                    }
+
+                }}
+                />
+            )
         })
 
     )
 }
+
+export default CardBoard
--- first_game/dapp-client/src/components/GameContainer/index.js
+++ first_game/dapp-client/src/components/GameContainer/index.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import CardBoard from "../CardBoard";
 
 const GameContainer = (props) => {
 
