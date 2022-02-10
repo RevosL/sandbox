777701f2ce3023afd61020fcc3df280f93b17b45
Bruno <planet.gamesextreme@gmail.com>
2022-02-10 16:03:46 -0300

    Update index.js

--- first_game/dapp-client/src/blockchain/index.js
+++ first_game/dapp-client/src/blockchain/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import React, { useState, useContext } from 'react'
+import { useState, useContext } from 'react'
 import Web3 from 'web3'
+import MemoryToken from "./contracts/MemoryToken.json"
 
 const BlockchainContext = React.createContext();
 
@@ -9,6 +10,10 @@ export default BlockchainContext;
 export const BlockchainProvider = ({children}) => {
 
     const [account, setAccount] = useState()
+    const [balance, setBalance] = useState(0)
+    const [token, setToken] = useState();
+
+    const [myTokens, setMyTokens] = useState([])
 
     const loadWeb3 = async () => {
 
@@ -41,6 +46,13 @@ export const BlockchainProvider = ({children}) => {
         const networkId = await web3.eth.net.getId()
         const networkData = MemoryToken.networks[networkId]
 
+        if(networkData) {
+            const abi = MemoryToken.abi
+            const address = networkData.address
+
+            const balance = web3.utils.fromWei(await web3.eth.getBalance(address), 'ether')
+            const _token = new web3.eth.Contract(abi, address)
+        }
     }
 
     return (
