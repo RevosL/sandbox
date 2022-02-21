dea7157d09eb766bf22ca08a563610dbddc992b6
Bruno <planet.gamesextreme@gmail.com>
2022-02-21 16:36:50 -0300

    LoginData

--- first_game/dapp-cliente-fix/src/components/GameContainer/index.js
+++ first_game/dapp-cliente-fix/src/components/GameContainer/index.js
@@ -2,8 +2,9 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Creators as MemoryActions } from "../../store/ducks/memory";
 
-import { Container, Row, Col } from "react-bootstrap";
-import CardBoard from "../CardBoard";
+import { Container, Row, Col } from 'react-bootstrap'
+import CardBoard from '../CardBoard'
+import CardListView from "../CardListView";
 import { Button } from "bootstrap";
 import { useBlockchain } from '../../blockchain';
 
@@ -25,6 +26,8 @@ const GameContainer = (props) => {
     const  handleOnWonCard = (cardId, cardUrl) => {
         console.log(cardId)
         console.log(cardUrl)
+
+        doMint(cardId, cardUrl)
     }
 
     return(
@@ -34,20 +37,18 @@ const GameContainer = (props) => {
             </Row>
             {!isLoged ?
                 <Row><Button onClick={handleLogin}>Login</Button></Row> :
-                <Row>
-                    <Col md={{ span: 4, offset: 4 }}>
-                        <CardBoard {...props} onWonCard={handleOnWonCard} ></CardBoard>
-                    </Col>
-                </Row>
+
+                <div>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <CardBoard {...props} onWonCard={handleOnWonCard} ></CardBoard>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <CardListView tokens={myTokens}></CardListView>
+                    </Row>
+                </div>
             }
-            <Row>
-                <Col md={{ span: 4, offset: 4 }}>
-                    <CardBoard {...props} onWonCard={handleOnWonCard}  ></CardBoard>
-                </Col>
-            </Row>
-            <Row>
-                <h2 className="text-center" style={{padding:"20px"}}>Tokens Collected: </h2>
-            </Row>
         </Container>
     )
 
